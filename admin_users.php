8d071c533996f50435412b87e0ec34fc3159f734
Kaith Neilson T. Dizon <kaith.dizon02082006@gmail.com>
2026-05-04 14:42:21 +0800

    Update admin_users.php

--- admin_users.php
+++ admin_users.php
@@ -62,7 +62,7 @@ if (!$result) {
                     <li class="nav-item"><a href="admin_catalog.php"><i class="fi fi-rr-search"></i> Catalog</a></li>
                     <li class="nav-item active"><a href="admin_users.php"><i class="fi fi-rr-users-alt"></i> Users</a></li>
                     <li class="nav-item"><a href="admin_waitlist.php"><i class="fi fi-rr-clock"></i> Waitlist</a></li>
-                    <li class="nav-item"><a href="admin_settings.php"><i class="fi fi-rr-settings"></i> Settings</a></li>
+                    <li class="nav-item"><a href="admin_settings.php"><i class="fi fi-rr-settings"></i> Fine Settings</a></li>
                 </ul>
             </nav>
             <div class="sidebar-footer">
